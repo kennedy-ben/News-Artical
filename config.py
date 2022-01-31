d288c3e5139f726a72fb088e58a96eaaf3c781ca
kennedy bernard <makuanikennedy@gmail.com>
2022-01-31 21:46:09 +0300

    added configrations

--- config.py
+++ config.py
@@ -1,11 +1,35 @@
-import OS
-#This module will allow application to interact with operating system
-class confi:
+import os
+
+class Config:
     """
-    config class  will contain all data that will be usedin development of our project
+    Config class will contain all(general) configurations/optimization
+    that will will be used in Development stage and Production class.
     """
-    NEWS_API_KEY = os.enviroment.get('NEWS_API_KEY')
-    SOURCES_BASE_API_URL ="http://newsapi.org/v2/sources?apikey={}"
-    EVERYTHING_BASE_API_URL = "http://newsapi.org/v2/everything?domains=wsj.com&apikey={}"
-    TOP_HEADLINES_BASE_API_URL = "http://newsapi.org/v2/headlines?sources={}&apikey={}"
-    FOOTBALL_TOP_HEADLINES = "http://newsapi.org/v2/football_headlines?country=us&category=football&apikey={}"
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SOURCES_BASE_API_URL = "https://newsapi.org/v2/sources?apiKey={}"
+    EVERYTHING_BASE_API_URL = "https://newsapi.org/v2/everything?domains=wsj.com&apikey={}"
+    TOP_HEADLINES_BASE_API_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    BUSINESS_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}"
+
+
+class ProdConfig(Config):
+    """    
+    This sub-class will also contain all the other configurations to
+    facilitate the production class. 
+    
+    """
+    pass
+
+
+class DevConfig(Config):
+    """
+    Sub-class contains all configurations that will facilate the development stage.
+    
+          
+    """
+  
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
